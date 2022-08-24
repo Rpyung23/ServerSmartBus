9c3387ed9decf44699759fd3a4c8eabf893752e4
Rpyung23 <guaman1579@gmail.com>
2022-08-24 16:48:21 -0500

    ads

--- app.js
+++ app.js
@@ -10,17 +10,17 @@ server.on('connection', (socket)=>
     {
         console.log(data)
         console.log(data[0])
-        if(data[0] == 'f1')
+        if(data[0].toString(16) == 'f1')
         {
-            console.log('LONGITUD TRAMA : '+data[1])
-            console.log('SERIE : '+data[2])
-            console.log('VELOCIDAD : '+parseInt(data[3],16))
-            console.log('ORIENTACION : '+parseInt(data[4],16))
-            console.log('SATELITES : '+parseInt(data[5],16))
-            console.log('FECHA : '+parseInt(data[6],16))
-            console.log('LAT : '+parseInt(data[7],16))
-            console.log('LNG : '+parseInt(data[8],16))
-            console.log('BANDERA : '+(parseInt(data[9], 16).toString(2)).padStart(8, '0'))
+            console.log('LONGITUD TRAMA : '+data[1].toString(16))
+            console.log('SERIE : '+data[2].toString(16))
+            console.log('VELOCIDAD : '+parseInt(data[3].toString(16),16))
+            console.log('ORIENTACION : '+parseInt(data[4].toString(16),16))
+            console.log('SATELITES : '+parseInt(data[5].toString(16),16))
+            console.log('FECHA : '+parseInt(data[6].toString(16),16))
+            console.log('LAT : '+parseInt(data[7].toString(16),16))
+            console.log('LNG : '+parseInt(data[8].toString(16),16))
+            console.log('BANDERA : '+(parseInt(data[9].toString(16), 16).toString(2)).padStart(8, '0'))
         }
 
         //console.log('El cliente ' + socket.remoteAddress + ":" + socket.remotePort + " dice: " + data)

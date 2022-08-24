4a1b01507fa288fcbbeb68e78f144198fad75bde
Rpyung23 <guaman1579@gmail.com>
2022-08-24 17:30:04 -0500

    asd

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 
 const net = require('net');
+const {parse} = require("nodemon/lib/cli");
 
 const server = net.createServer()
 
@@ -8,18 +9,24 @@ server.on('connection', (socket)=>
     console.log('CLINETE CONECTADO ')
     socket.on('data', (data)=>
     {
+        var fecha = ""
+        var lat = ""
+        var lng = ""
         console.log(data)
         console.log(data[0])
         if(data[0].toString(16) == 'f1')
         {
+            fecha = data[8].toString(16)+data[9].toString(16)+data[10].toString(16)+data[11].toString(16)
+            lat = data[12].toString(16)+data[13].toString(16)+data[14].toString(16)+data[15].toString(16)
+            lng = data[16].toString(16)+data[17].toString(16)+data[18].toString(16)+data[19].toString(16)
             console.log('LONGITUD TRAMA : '+data[1].toString(16))
             console.log('SERIE : '+data[2].toString(16)+data[3].toString(16)+data[4].toString(16))
             console.log('VELOCIDAD : '+parseInt(data[5].toString(16),16))
             console.log('ORIENTACION : '+parseInt(data[6].toString(16),16))
             console.log('SATELITES : '+parseInt(data[7].toString(16),16))
-            console.log('FECHA : '+data[8].toString(16)+data[9].toString(16)+data[10].toString(16)+data[11].toString(16))
-            console.log('LAT : '+data[12].toString(16)+data[13].toString(16)+data[14].toString(16)+data[15].toString(16))
-            console.log('LNG : '+data[16].toString(16)+data[17].toString(16)+data[18].toString(16)+data[19].toString(16))
+            console.log('FECHA : '+parseInt(fecha).toString(16))
+            console.log('LAT : '+parseFloat(lat).toString(16))
+            console.log('LNG : '+parseFloat(lng).toString(16))
             console.log('BANDERA : '+(parseInt(data[20].toString(16), 16).toString(2)).padStart(8, '0'))
         }
 

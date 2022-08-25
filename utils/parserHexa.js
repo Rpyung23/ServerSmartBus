f8de4c0a9df03ec7997e54c43867145d69426992
Rpyung23 <guaman1579@gmail.com>
2022-08-24 22:02:40 -0500

    asd

--- utils/parserHexa.js
+++ utils/parserHexa.js
@@ -4,15 +4,22 @@ function decimaltoHexa(dato)
     return (hex.length < 2 ? "0"+hex : hex)
 }
 
-function HexToSignedInt(num, numSize) {
-    var val = {
-        mask: 0x8 * Math.pow(16, numSize-1), //  0x8000 if numSize = 4
-        sub: -0x1 * Math.pow(16, numSize)    //-0x10000 if numSize = 4
-    }
-    if((parseInt(num, 16) & val.mask) > 0) { //negative
-        return (val.sub + parseInt(num, 16))
-    }else {                                 //positive
-        return (parseInt(num,16))
-    }
+function HexToSignedInt(hex)
+{
+    var data = hex.match(/../g);
+
+// Create a buffer
+    var buf = new ArrayBuffer(4);
+// Create a data view of it
+    var view = new DataView(buf);
+
+// set bytes
+    data.forEach(function (b, i) {
+        view.setUint8(i, parseInt(b, 16));
+    });
+
+// get an int32 with little endian
+    var num = view.getInt32(0, 1);
+    return num
 }
 module.exports = {decimaltoHexa,HexToSignedInt}

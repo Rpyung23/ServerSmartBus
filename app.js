7c1f2bd54dfe277666fcc95c9e7689e7d4c13892
Rpyung23 <guaman1579@gmail.com>
2024-03-20 17:51:58 -0500

    update

--- app.js
+++ app.js
@@ -1,41 +1,28 @@
 // 1*60*60*1000
 //const TIMEOUT_1HORA = (1*60*60*1000)
-const net = require('net');
-const ControllerTramaSocket = require("./controller/controller.tramaSocket")
 const {convertBufferToHex} = require("./utils/parserHexa")
-// Importa la clase SingletonEquipoGps desde el archivo donde la hayas definido.
-const SingletonEquipoGps  = require('./models/SingletonEquipoGps');
+const net = require('net');
 const server = net.createServer()
-// Obtén la instancia de la clase.
-const oSingletonEquipoGps  = new SingletonEquipoGps();
-
 server.on('connection', (socketClient)=>
 {
     console.log('NUEVO CLIENTE CONECTADO '+socketClient.remoteAddress)
 
     socketClient.on('data', (data)=>
     {
-        if(data.toString().includes('\ufffd'))
+        console.log(data)
+        /**DATOS RECIBOS DESDE EQUIPO GPS**/
+        try{
+            var serie = convertBufferToHex(data[2])+convertBufferToHex(data[3])+convertBufferToHex(data[4])
+            console.log(`SERIE ${serie}`)
+        }catch (e) {
+            console.log(e)
+        }
+
+        /*if(data.toString().includes('\ufffd'))
         {
-            /**DATOS RECIBOS DESDE EQUIPO GPS**/
-            try{
-                var serie = convertBufferToHex(data[2])+convertBufferToHex(data[3])+convertBufferToHex(data[4])
-                oSingletonEquipoGps.agregarEquipo(serie,socketClient,data)
-                let equipo = oSingletonEquipoGps.obtenerEquipoPorSerie(serie)
-                if (equipo.isF0 && equipo.isB2)
-                {
-                    if(!equipo.socketEquipo.connecting){
-                        equipo.socketEquipo.write(".trackOK.")
-                        console.log("ENVIADO .trackOK.")
-                    }
-                }
-                ControllerTramaSocket.registerControllerTramaSocket(serie,data)
-            }catch (e) {
-                console.log(e)
-            }
+
 
         }else{
-            /**DATOS RECIBOS NETWORK**/
             console.log(data.toString())
             var asciiDatos = data.toString().split(',')
             let equipo = oSingletonEquipoGps.obtenerEquipoPorSerie(asciiDatos[0].toString())
@@ -46,7 +33,7 @@ server.on('connection', (socketClient)=>
             }else{
                 console.log("EQUIPO NO ENCONTRADO")
             }
-        }
+        }*/
     })
 
     socketClient.on('close', ()=>{
